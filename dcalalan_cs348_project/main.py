c6c2b3a7411b3d296026ef0a6507beee4da63918
DavidCalalang <dcalalan@purdue.edu>
2024-10-25 21:04:16 -0400

    table shows

--- dcalalan_cs348_project/main.py
+++ dcalalan_cs348_project/main.py
@@ -14,6 +14,7 @@ import models
 from database import engine, session_local
 from sqlalchemy.orm import Session
 import pandas as pd
+import duckdb
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
@@ -66,16 +67,8 @@ async def read_user(user_id: int, db:db_dependency):
                 raise HTTPException(status_code=404, detail="User not found")
         else:
                 return user
-        
-@app.get("/all_listeners", status_code=status.HTTP_200_OK)
-async def list_all_users(db:db_dependency):
-        all_listeners = db.query(models.Listeners).all()
 
-        for listener in all_listeners:
-                print(listener.username)
-
-        return {"data": all_listeners}
-
+"""
 @app.get("/all_artists", status_code=status.HTTP_200_OK)
 async def list_all_artists(db:db_dependency):
         all_artists = db.query(models.Artists).all()
@@ -84,8 +77,9 @@ async def list_all_artists(db:db_dependency):
                 print(artist.artist_name)
 
         return {"data": all_artists}
-
-@app.get("/all_tracks", status_code=status.HTTP_200_OK)
+"""
+        
+@app.get("/", status_code=status.HTTP_200_OK)
 async def list_all_tracks(db:db_dependency):
         all_tracks = db.query(models.Tracks).all()
 
@@ -103,8 +97,62 @@ async def list_all_tracks(db:db_dependency):
         # To convert to table to show with html
         print(df)
 
-        return {"data": all_tracks}
+        df.to_html('./html_files/basic.html', index=False)
 
+        with open('./html_files/main.html', 'r') as f:
+                test = f.read()
+
+        return HTMLResponse(content=test, status_code=200)
+
+@app.get("/main_table", response_class=HTMLResponse)
+async def main_table():
+    try:
+        # Read main.html
+        with open("./html_files/main.html", "r") as main_file:
+            main_content = main_file.read()
+        
+        # Read basic.html
+        with open("./html_files/basic.html", "r") as basic_file:
+            basic_content = basic_file.read()
+            
+        # Find the closing </body> tag and insert basic_content before it
+        merged_content = main_content.replace("</body>", f"""
+            <div class="data-content">
+                {basic_content}
+            </div>
+        </body>""")
+            
+        return merged_content
+            
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=404, detail=f"File not found: {str(e)}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading files: {str(e)}")
+
+@app.get("/test_sql", status_code=status.HTTP_200_OK)
+async def test_sql(db:db_dependency):
+        all_tracks = db.query(models.Tracks).all()
+
+        # Create an empty DataFrame with the desired columns
+        df = pd.DataFrame(columns=['track_id', 'track_name', 'album_id', 'artist_id', 'genre'])
+
+        track_dfs = [pd.DataFrame({'track_id': track.track_id,
+                                'track_name': track.track_name,
+                                'album_id': track.album_id,
+                                'artist_id': track.artist_id,
+                                'genre': track.genre}, index=[0]) for track in all_tracks]
+
+        df = pd.concat(track_dfs, ignore_index=True)
+
+        ye = 3
+        foo = duckdb.query("SELECT * FROM df WHERE artist_id = " + str(ye)).df()
+        foo.to_html('./html_files/basic.html', index=False)
+        print(foo)
+
+        with open('./html_files/basic.html', 'r') as f:
+                test = f.read()
+
+        return HTMLResponse(content=test, status_code=200)
 
 """
 # Opens .html files for rendering in various pages.
